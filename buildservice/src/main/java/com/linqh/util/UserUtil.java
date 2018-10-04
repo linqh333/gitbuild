f01f05d99063a8fd4a3bc278a2ebe5d051c802ef
linqh <linqh@newlandcomputer.com>
2018-10-04 10:59:30 +0800

    出事化对象

--- buildservice/src/main/java/com/linqh/util/UserUtil.java
+++ buildservice/src/main/java/com/linqh/util/UserUtil.java
@@ -1,7 +1,10 @@
 package com.linqh.util;
 
+import com.linqh.model.User;
+
 public class UserUtil {
     public static void main(String[] args) {
+        User user = new User();
         System.out.println("11111");
     }
 }
